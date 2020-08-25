cf6c14cdfd030b50af9e6662e25c05a148ffae1f
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-08-26 00:18:06 +0500

    Add project files.

---
+++ src/MediatR.Helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediatR.Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
